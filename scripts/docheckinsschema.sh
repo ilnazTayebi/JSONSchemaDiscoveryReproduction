be7b0e19f9c2d82523bded705bcb805f025c1511
ilnazTayebi <ilnaz.tayebi@hotmail.com>
2023-02-06 23:36:51 +0100

    Git Cherry-Pick makes last commits wrong. Fixed them
    Git Cherry-Pick makes last commits wrong. Fixed them

--- scripts/docheckinsschema.sh
+++ scripts/docheckinsschema.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-
 host=localhost
 port=3000
 
--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!bin/sh
 python2.7 /home/repro/prepare_data.py > /home/repro/paper/tablefoursquare.tex
 pdflatex /home/repro/paper/report.tex
